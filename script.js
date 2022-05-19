6c40086fdf3149e1002198977ded5f52d6000b9e
Interrubble <delagarzachris@icloud.com>
2022-05-18 18:29:02 -0700

    added city search functionality

--- script.js
+++ script.js
@@ -181,3 +181,78 @@ $('#search').on("click", function (event) {
     fetchLatLon();
     $('#searchbox').val('');    
 })
+
+// City button click event
+buttonList.on("click", function (event) {
+    var buttonID = event.target.id;
+    city = buttonID; 
+    function fetchLatLon() {
+        var requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=92dcd99075e42747c6a9b09497364593";
+    
+        fetch(requestURL)
+            .then(function (response) {
+                if (!response.ok) {
+                alert('Please enter a valid city.');
+                throw response.json();
+                } 
+            return response.json();
+            })
+
+            .then(function(data) {
+                var latitude = data.coord.lat;
+                var longitude = data.coord.lon;
+
+                function fetchForecast() {
+
+                    var forecastRequest = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + APIKey;
+                
+                    fetch(forecastRequest)
+                        .then(function (response) {
+                            if (!response.ok) {
+                            throw response.json();
+                            } 
+                        return response.json();
+                        })
+                        .then(function(data) {
+                            // Today's weather
+                            var currentTemp = data.current.temp + "\xB0 F";
+                            var currentWind = data.current.wind_speed + " MPH";
+                            var currentHumidity = data.current.humidity + "%";
+                            var currentUVI = data.current.uvi;
+                            var iconCode = data.current.weather[0].icon;
+
+                            var iconURL = 'https://openweathermap.org/img/wn/' + iconCode + '@2x.png';
+                            
+                            var icon = $(`<img id="icon" alt="weather icon">`);
+                            icon.attr('src', iconURL);
+                            var spanIcon = $('<span>');
+                            spanIcon.append(icon);
+
+                            postCurrentWeather(city, currentTemp, currentWind, currentHumidity, currentUVI, iconCode);
+
+                            // 5-day forecast
+                            var dailyForecast = data.daily;
+                            forecastCardDiv.empty();
+                            for (let i = 1; i < 6; i++) {
+                                let currentDay = dailyForecast[i];
+                                var unix = currentDay.dt;
+                                var forecastedDate = moment.unix(unix).format('M/DD/YYYY');
+                                var forecastedTemp = currentDay.temp.day  + "\xB0 F";
+                                var forecastedWind = currentDay.wind_speed + " MPH";
+                                var forecastedHumidity = currentDay.humidity + " %";
+                                var forecastedIcon = currentDay.weather[0].icon;
+
+                                var forecastIconURL = 'https://openweathermap.org/img/wn/' + forecastedIcon + '@2x.png';
+
+                                var iconEl = $(`<img id="icon" alt="weather icon">`);
+                                iconEl.attr('src', forecastIconURL);
+
+                                createForecastCard(forecastedDate, forecastedTemp, forecastedWind, forecastedHumidity, iconEl)
+                            }
+                        })
+                }
+                fetchForecast();
+            })
+    }
+    fetchLatLon()
+})    
